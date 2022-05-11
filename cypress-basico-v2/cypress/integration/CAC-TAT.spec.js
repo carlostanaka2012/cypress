2e8375943d52929115cded5eb2c9bcf22dd191bc
carlostanaka2012 <carlostanaka2012@gmail.com>
2022-05-11 08:15:22 -0300

    Aula3 exe2
    Aula3 exe2

---
+++ cypress-basico-v2/cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,34 @@
+// CAC-TAT.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+   beforeEach(function(){
+      cy.visit('./src/index.html')
+   })
+
+   it('verifica o título da aplicação', function() {
+      cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+ 
+   })
+
+   it.only('preenche os campos obrigatórios e envia formul´rio', function() {
+
+      const longText = 'Teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,'
+
+      cy.get('#firstName').type('CARLOS')
+      cy.get('#lastName').type('Tanaka')
+      cy.get('#email').type('tanaka@qa.com.br')
+      cy.get('#open-text-area').type(longText, { delay: 0})
+      cy.get('button[type="submit"]').click()
+
+      cy.get('.success').should('be.visible')
+
+   })
+ })
+
