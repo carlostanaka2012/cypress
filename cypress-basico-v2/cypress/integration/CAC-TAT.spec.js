fbf308fab15f44efe7aca9c39dd47b691eca3211
carlostanaka2012 <carlostanaka2012@gmail.com>
2022-05-11 08:32:08 -0300

    Aula4 ex3 error
    Aula4 ex3 error

--- cypress-basico-v2/cypress/integration/CAC-TAT.spec.js
+++ cypress-basico-v2/cypress/integration/CAC-TAT.spec.js
@@ -17,7 +17,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
  
    })
 
-   it.only('preenche os campos obrigatórios e envia formul´rio', function() {
+   it('preenche os campos obrigatórios e envia formul´rio', function() {
 
       const longText = 'Teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,'
 
@@ -29,6 +29,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
       cy.get('.success').should('be.visible')
 
+   })
+   it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+      cy.get('#firstName').type('CARLOS')
+      cy.get('#lastName').type('Tanaka')
+      cy.get('#email').type('tanaka@qa,com.br')
+      cy.get('#open-text-area').type('Teste')
+      cy.get('button[type="submit"]').click()
+
+      cy.get('.error').should('be.visible')
    })
  })
 
