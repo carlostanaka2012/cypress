ce3c18b7073650dfb4750416410ca1d4865df610
carlostanaka2012 <carlostanaka2012@gmail.com>
2022-06-03 08:04:51 -0300

    Aula 53 ex 52 encontre o gato
    implementação de testes completos

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -296,7 +296,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
  })
    
 
-  it.only('encontra o gato escondido', function(){
+  it('encontra o gato escondido', function(){
       cy.get('#cat')
          .invoke('show')
           .should('be.visible')
